cb71b7879c1ff64b3491d21e704b81ae76cefabe
Muhammad Dhalan <dhalanscout7423@gmail.com>
2025-12-04 01:14:19 +0800

    feat: add new migrations

---
+++ database/migrations/2025_12_01_033835_create_documents_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('documents', function (Blueprint $table) {
+            $table->ulid('id')->primary();
+            $table->foreignUlid('project_id')->nullable()->index();
+            // $table->foreignUlid('rincian_id')->nullable()->index();
+            $table->foreignUlid('pic_id')->nullable()->index();
+            $table->string('title');
+            $table->string('document_number')->nullable();
+            $table->string('status')->nullable();
+            $table->text('description')->nullable();
+            $table->foreignUlid('created_by')->nullable()->index();
+            $table->json('meta')->nullable();
+            $table->softDeletes();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('documents');
+    }
+};
---
+++ database/migrations/2025_12_02_084347_create_document_rincian_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('document_rincian', function (Blueprint $table) {
+            $table->id();
+            $table->foreignUlid('document_id')->nullable()->constrained('documents')->cascadeOnDelete();
+            $table->foreignUlid('rincian_id')->nullable()->constrained('rincians')->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('document_rincian');
+    }
+};
