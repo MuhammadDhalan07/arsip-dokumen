2dab92458eac338fb3178181b3196cb4dd42210b
Muhammad Dhalan <dhalanscout7423@gmail.com>
2025-12-17 17:04:47 +0800

    wip: DocumentExport

---
+++ app/Exports/DocumentExport.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Exports;
+
+use App\Models\Document;
+use Maatwebsite\Excel\Concerns\FromCollection;
+
+class DocumentExport implements FromCollection
+{
+    /**
+    * @return \Illuminate\Support\Collection
+    */
+
+    public function make()
+    {
+        
+    }
+    public function collection()
+    {
+        return Document::all();
+    }
+}
