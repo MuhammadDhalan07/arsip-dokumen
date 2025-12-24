e3f2bed5d4a6026762b528bc089054c191cad1c6
Muhammad Dhalan <dhalanscout7423@gmail.com>
2025-12-24 10:10:34 +0800

    feat: add action export in documen

--- app/Filament/Resources/Documents/Pages/ListDocuments.php
+++ app/Filament/Resources/Documents/Pages/ListDocuments.php
@@ -2,7 +2,10 @@
 
 namespace App\Filament\Resources\Documents\Pages;
 
+use App\Exports\DocumentExport;
 use App\Filament\Resources\Documents\DocumentResource;
+use App\Models\Document;
+use Filament\Actions\Action;
 use Filament\Actions\CreateAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -13,6 +16,15 @@ class ListDocuments extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
+            Action::make('export')
+                ->label('Export Dokumen')
+                ->color('success')
+                ->icon('fileicon-microsoft-excel')
+                ->action(function () {
+                    $document = Document::all();
+
+                    return DocumentExport::make($document);
+                }),
             CreateAction::make(),
         ];
     }
