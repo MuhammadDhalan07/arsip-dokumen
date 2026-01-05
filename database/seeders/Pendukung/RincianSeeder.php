3fb3e1afa16f929195ea1d437718245d225dc3d0
Muhammad Dhalan <dhalanscout7423@gmail.com>
2026-01-05 10:37:38 +0800

    update rincianseeder

--- database/seeders/Pendukung/RincianSeeder.php
+++ database/seeders/Pendukung/RincianSeeder.php
@@ -18,61 +18,72 @@ class RincianSeeder extends Seeder
             [
                 'name' => 'Dokumen penawaran',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 10.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'Laporan',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 15.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'Panduan',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 10.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'Permohonan Pemeriksaan dan Serah Terima',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'Permohonan Pembayaran',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 8.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'RAB',
                 'type' => JenisRincian::INTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'KAK',
                 'type' => JenisRincian::EKSTERNAL,
+                'bobot' => 15.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'HPS',
                 'type' => JenisRincian::EKSTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'SPK',
                 'type' => JenisRincian::EKSTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'SPMK',
                 'type' => JenisRincian::EKSTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ],
             [
                 'name' => 'BAST',
                 'type' => JenisRincian::EKSTERNAL,
+                'bobot' => 7.00,
                 'is_active' => true,
             ]
         ];
         foreach ($data as $item) {
-            Rincian::firstOrCreate(
+            Rincian::updateOrCreate(
                 ['name' => $item['name']],
                 $item
             );
